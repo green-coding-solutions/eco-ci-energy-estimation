bf8092edac51b3160796cf0101115bfb9b0b55ca
dan-mm <dan@green-coding.berlin>
2023-06-13 13:17:30 +0200

    add different output for gitlab/github

--- scripts/display_results.sh
+++ scripts/display_results.sh
@@ -28,15 +28,12 @@ function display_results {
     fi
 
     if [[ ${display_table} == 'true' ]]; then
-        echo "|Label|🖥 avg. CPU utilization [%]|🔋 Total Energy [Joules]|🔌 avg. Power [Watts]|Duration [Seconds]|" >> $output
-        echo "|---|---|---|---|---|" >> $output
-
         cpu_avg=$(awk '{ total += $1; count++ } END { print total/count }' /tmp/eco-ci/cpu-util-total.txt)
         total_energy=$(awk '{sum+=$1} END {print sum}' /tmp/eco-ci/energy-total.txt)
         power_avg=$(awk '{ total += $1; count++ } END { print total/count }' /tmp/eco-ci/energy-total.txt)
         time=$(($(date +%s) - $(cat /tmp/eco-ci/timer-total.txt)))
-        echo "|Total Run|$cpu_avg|$total_energy|$power_avg|$time|" >> $output
 
+        # get series of measurement values
         for varname in ${!measurement_@}; do
           # Extract the measurement number from the variable name
           measurement_number="${varname#*_}"
@@ -53,22 +50,50 @@ function display_results {
           max_measurement_number=$measurement_number
         done
 
-        #display measurument lines in table summary
-        for (( i=1; i<=$max_measurement_number; i++ ))
-        do
-            echo "|$(eval echo \$label_$i)|$(eval echo \$cpu_avg_$i)|$(eval echo \$total_energy_$i)|$(eval echo \$power_avg_$i)|$(eval echo \$time_$i)|" >> $output
-        done
+        if [[ $source == 'github' ]]; then
+            echo "|Label|🖥 avg. CPU utilization [%]|🔋 Total Energy [Joules]|🔌 avg. Power [Watts]|Duration [Seconds]|" >> $output
+            echo "|---|---|---|---|---|" >> $output
+            echo "|Total Run|$cpu_avg|$total_energy|$power_avg|$time|" >> $output
+            #display measurument lines in table summary
+            for (( i=1; i<=$max_measurement_number; i++ ))
+            do
+                echo "|$(eval echo \$label_$i)|$(eval echo \$cpu_avg_$i)|$(eval echo \$total_energy_$i)|$(eval echo \$power_avg_$i)|$(eval echo \$time_$i)|" >> $output
+            done
 
-        # echo -e "$final_line" >> $output
-        echo '' >> $output
+            # echo -e "$final_line" >> $output
+            echo '' >> $output
+        elif [[ $source == 'gitlab' ]]; then
+            echo "Total Energy [Joules]: $total_energy" >> $output
+            echo "Total Avg. CPU Utilization: $cpu_avg" >> $output
+            echo "Total Avg. Power [Watts]: $power_avg" >> $output
+            echo "Total Duration [seconds]: $time" >> $output
+            echo "----------------" >> $output
+
+            for (( i=1; i<=$max_measurement_number; i++ )); do
+                echo "Label $i: $(eval echo \$label_$i)" >> $output
+                echo "Energy Used [Joules]: $(eval echo \$total_energy_$i)" >> $output
+                echo "Avg. CPU Utilization: $(eval echo \$cpu_avg_$i)" >> $output
+                echo "Avg. Power [Watts]: $(eval echo \$power_avg_$i)" >> $output
+                echo "Duration [seconds]: $(eval echo \$time_$i)" >> $output
+                echo "----------------" >> $output
+            done
+        fi
+
+
+
     fi
 
     if [[ ${display_graph} == 'true' ]]; then
-        echo '📈 Energy graph:' >> $output
-        echo '```bash' >> $output
-        echo ' ' >> $output
-        cat /tmp/eco-ci/energy-total.txt | /home/runner/go/bin/asciigraph -h 10 -c "Watts over time" >> $output
-        echo ' ```' >> $output
+        if [[ $source == 'github' ]]; then
+            echo '📈 Energy graph:' >> $output
+            echo '```bash' >> $output
+            echo ' ' >> $output
+            cat /tmp/eco-ci/energy-total.txt | /home/runner/go/bin/asciigraph -h 10 -c "Watts over time" >> $output
+            echo ' ```' >> $output
+        elif [[ $source == 'gitlab' ]]; then
+            echo '📈 Energy graph:' >> $output
+            cat /tmp/eco-ci/energy-total.txt | /home/runner/go/bin/asciigraph -h 10 -c "Watts over time" >> $output
+        fi
     fi
 
     repo_enc=$( echo ${repo} | jq -Rr @uri)
@@ -78,7 +103,7 @@ function display_results {
         get_endpoint=$API_BASE"/v1/ci/measurement/get"
         metrics_url="https://metrics.green-coding.berlin"
 
-        echo "Badge for your README.md" >> $output
+        echo "Badge for your README.md:" >> $output
         echo ' ```' >> $output
         echo "[![Energy Used](${get_endpoint}?repo=${repo_enc}&branch=${branch_enc}&workflow=$WORKFLOW_ID)](${metrics_url}/ci.html?repo=${repo_enc}&branch=${branch_enc}&workflow=$WORKFLOW_ID)" >> $output
         echo ' ```' >> $output
@@ -125,6 +150,10 @@ while [[ $# -gt 0 ]]; do
         ;;
         -sd|--send-data) 
         send_data="$2"
+        shift
+        ;;
+        -s|--source)
+        source="$2"
         shift
         ;;
         \?) echo "Invalid option -$1" >&2
