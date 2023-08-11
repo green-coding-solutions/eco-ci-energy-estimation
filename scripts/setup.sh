63aff46cf1b3a4159819e5fc11457a141b434b0d
dan-mm <dan@green-coding.berlin>
2023-08-11 10:37:51 +0200

    add ngrok for testing

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -60,7 +60,8 @@ function start_measurement {
     # call init_variables
     source "$(dirname "$0")/vars.sh" cpu_vars
 
-    source "$(dirname "$0")/vars.sh" add_var API_BASE "https://api.green-coding.berlin"
+    # source "$(dirname "$0")/vars.sh" add_var API_BASE "https://api.green-coding.berlin"
+    source "$(dirname "$0")/vars.sh" add_var API_BASE "https://8ba0-2a02-2454-4e0-ba00-afa6-917d-2bad-a296.ngrok-free.app"
     source "$(dirname "$0")/vars.sh" add_var INIT "DONE"
 
     # start measurement
