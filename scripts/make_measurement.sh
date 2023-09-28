4af24ed5542fbdd6ce2beeb4d6df78d6cc7f867e
dan-mm <dan@green-coding.berlin>
2023-09-28 17:13:23 +0200

    debug create-and-add-meta call

--- scripts/make_measurement.sh
+++ scripts/make_measurement.sh
@@ -97,7 +97,7 @@ function make_measurement() {
         run_id_enc=$( echo ${run_id} | jq -Rr @uri)
 
         echo "show create-and-add-meta.sh output"
-        echo "--file $total_data_file --repository $repo_enc --branch $branch_enc --workflow $WORKFLOW_ID --run_id $run_id_enc"
+        echo "--file $lap_data_file --repository $repo_enc --branch $branch_enc --workflow $WORKFLOW_ID --run_id $run_id_enc"
 
         source "$(dirname "$0")/create-and-add-meta.sh" --file ${lap_data_file} --repository ${repo_enc} --branch ${branch_enc} --workflow $WORKFLOW_ID --run_id ${run_id_enc}
         source "$(dirname "$0")/add-data.sh" --file ${lap_data_file} --label "$label" --cpu ${cpu_avg} --energy ${total_energy} --power ${power_avg}
