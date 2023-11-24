d8de43edd9dcab0672e123aa148805a50f33c8c1
dan-mm <dan@green-coding.berlin>
2023-11-24 14:44:30 +0100

    ngrok showcase

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -62,7 +62,8 @@ function start_measurement {
     # call init_variables
     source "$(dirname "$0")/vars.sh" cpu_vars
 
-    source "$(dirname "$0")/vars.sh" add_var API_BASE "https://api.green-coding.berlin"
+    # source "$(dirname "$0")/vars.sh" add_var API_BASE "https://api.green-coding.berlin"
+    source "$(dirname "$0")/vars.sh" add_var API_BASE "https://8edd-62-214-9-98.ngrok-free.app"
     source "$(dirname "$0")/vars.sh" add_var INIT "DONE"
 
     # start measurement
