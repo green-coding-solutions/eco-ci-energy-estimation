2f6247691710aa6ab5ed8a786e8c10486e347636
Arne Tarara <arne@datafuse.de>
2025-04-11 09:53:24 +0200

    macOS Hotfix for CPU utilization (#120)

--- scripts/cpu-utilization-macos.sh
+++ scripts/cpu-utilization-macos.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # By ChatGPT and masterfully adapted by Arne Tarara. No rights reserved :)
 
-iostat -w 1 | awk -v date_cmd="gdate +%s%N" '
+iostat -w 1 -n 0 | awk -v date_cmd="gdate +%s%N" '
 NR > 3 { # skips first 3 rows, which contain header data and first average-only measurement
     # Extract user, system, and idle CPU percentages
-    user = $3
-    sys = $4
-    idle = $6
+    user = $1
+    sys = $2
+    idle = $3
 
     # Calculate total CPU usage
     usage = 100 - idle
@@ -25,7 +25,6 @@ NR > 3 { # skips first 3 rows, which contain header data and first average-only 
 
     # Print the time and CPU usage
     printf "%.10f %.2f\n", (time_diff_ns / 1000000000), usage
-
 
     # Store the current time as the last time for the next iteration
     last_time_ns = current_time_ns
