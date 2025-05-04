85124bb09a4f62b30e3f6b6da9e2df120df90a53
Arne Tarara <arne@datafuse.de>
2025-05-04 11:50:32 +0200

    Migrated error to a warning for data backfill

--- scripts/make_measurement.sh
+++ scripts/make_measurement.sh
@@ -91,9 +91,9 @@ function make_measurement() {
 
     if [[ $captured_datapoints -gt 0 ]]; then
         if [[ $captured_datapoints -lt $(($step_time_s_int - 1)) ]]; then # one datapoint might be missing due to the fact that we need to wait for one tick
-            ECO_CI_STEP_NOTE="Missing data points. Expected ${step_time_s_int} (-1) but got ${captured_datapoints}"
-            echo "Error! - " $ECO_CI_STEP_NOTE  >&2
-            [ -n "$GITHUB_STEP_SUMMARY" ] && echo "❌ Error! - $ECO_CI_STEP_NOTE" >> $GITHUB_STEP_SUMMARY
+            ECO_CI_STEP_NOTE="Missing data points. Expected ${step_time_s_int} (-1) but got ${captured_datapoints} - Data will be backfilled"
+            echo "Warning - " $ECO_CI_STEP_NOTE  >&2
+            [ -n "$GITHUB_STEP_SUMMARY" ] && echo "⚠️ Warning - $ECO_CI_STEP_NOTE" >> $GITHUB_STEP_SUMMARY
         fi
 
         # now we are backfilling data. this happens in any case as due to the low sampling rate of 1 seconds we will have
